cb48b65f790e1551db22d9a325181a7236a20563
paula.santana <paula.santana@zup.com.br>
2022-02-02 19:55:39 -0300

    Adicionando estrutura do projeto

---
+++ src/test/java/com/zupfood/pedidos/PedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zupfood.pedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
